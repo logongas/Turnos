5721f5c8dd6eb31ad8b7a9378f4f4da3d9334900
Lorenzo Gonzalez <logongas@gmail.com>
2012-03-27 23:20:54 +0200

    Quitar el main de la clase ConfigurarTurno

--- src/net/ausiasmarch/turnos/ConfigurarTurno.java
+++ src/net/ausiasmarch/turnos/ConfigurarTurno.java
@@ -171,22 +171,6 @@ public class ConfigurarTurno extends javax.swing.JDialog {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                ConfigurarTurno dialog = new ConfigurarTurno(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
